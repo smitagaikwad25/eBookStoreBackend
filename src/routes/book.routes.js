c6911534cd03582a123476ccb2f36fad0f75aadc
prasad98765 <pnchaudhari1996@gmail.com>
2020-03-18 19:22:11 +0530

    Added : install multer dependencies

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,28 +1,28 @@
 module.exports = app => {
   const BOOK_CONTROLLER = require("../controller/book.controller");
-  var multer = require("multer");
+  // var multer = require("multer");
 
-  const FILE_PATH = "uploads";
-  const upload = multer({
-    dest: `${FILE_PATH}/`
-  });
+  // const FILE_PATH = "uploads";
+  // const upload = multer({
+  //   dest: `${FILE_PATH}/`
+  // });
 
   app.post("/Books", BOOK_CONTROLLER.create);
   app.get("/getBooks", BOOK_CONTROLLER.getAllBooks);
 
-  app.post("/FilePath", upload.single("filePath"), (req, res) => {
-    const avatar = req.file;
-    console.log(avatar);
-    console.log(req.file.path);
-    if (!avatar) {
-      res.status(400).send({
-        status: false,
-        data: "No file is selected."
-      });
-    } else {
-      res.send(req.file.path);
-    }
-  });
+  // app.post("/FilePath", upload.single("filePath"), (req, res) => {
+  //   const avatar = req.file;
+  //   console.log(avatar);
+  //   console.log(req.file.path);
+  //   if (!avatar) {
+  //     res.status(400).send({
+  //       status: false,
+  //       data: "No file is selected."
+  //     });
+  //   } else {
+  //     res.send(req.file.path);
+  //   }
+  // });
 
   
   app.get('/searchBook',BOOK_CONTROLLER.searchBook)
